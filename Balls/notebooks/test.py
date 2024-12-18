788c4312116a5920181da6d6983ef688d3bb64d1
IAteMyOwnPoo <anolifeweeb@gmail.com>
2024-12-17 21:11:23 -0700

    Add files via upload

---
+++ Balls/notebooks/test.py
@@ -0,0 +1,6 @@
+import openai
+
+openai.api_key = "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"
+engine = "gpt-3.5-turbo"
+
+prompt = "hello chat"
